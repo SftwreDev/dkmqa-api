98a09cd7f830430643c125c9efce9aa6eab8b4f7
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2020-09-02 01:36:34 +0800

    Update settings.py

--- main_settings/settings.py
+++ main_settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#fx)*$$_kzktzop#f8lj$q%hphs3vu6=bg^satgny8bt8!-pdm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dkmqa-api.herokuapp.com']
 
 
 # Application definition
