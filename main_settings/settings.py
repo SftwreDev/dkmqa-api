620f3bfc083dc4a67a4fffa8e36e78b441a110e1
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2020-09-02 02:32:01 +0800

    Update settings.py

--- main_settings/settings.py
+++ main_settings/settings.py
@@ -90,24 +90,24 @@ WSGI_APPLICATION = 'main_settings.wsgi.application'
 
 ######## DB.SQLite3 ########
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-###### DB.MySQL #####
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dkm-database-test',
-        'USER': 'main',
-        'PASSWORD': 'password...',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+###### DB.MySQL #####
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dkm-database-test',
+#         'USER': 'main',
+#         'PASSWORD': 'password...',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
