99a0e81580f6133a21e9dfcbae90b11b49708963
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2020-09-09 21:03:16 +0800

    Update settings.py

--- main_settings/settings.py
+++ main_settings/settings.py
@@ -199,7 +199,7 @@ DJOSER = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     # 'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     # 'ROTATE_REFRESH_TOKENS': False,
     # 'BLACKLIST_AFTER_ROTATION': True,
